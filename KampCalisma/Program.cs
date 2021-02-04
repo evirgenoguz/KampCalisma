f0b9299c7558bf54421a0a9d4238610f364cd2dd
Oğuz EVİRKEN <oguzevirken@marun.edu.tr>
2021-02-05 01:23:47 +0300

    Add project files.

---
+++ KampCalisma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KampCalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // naming convention
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepete Eklendi");
+
+        }
+        public void Ekle2(string urunAdi, string aciiklama, double fiyat)
+        {
+            Console.WriteLine(urunAdi + " Sepete Eklendi");
+        }
+    }
+}
