982491cd342026d4aff67d5863b1e4e282bb6d84
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-02-06 15:21:46 +0300

    Kampın 4. Gün Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Oguz", "Ali", "Veli", "Ayşe" };
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Oguz", "Ali", "Veli", "Ayşe" };
+            Console.WriteLine(isimler2[0]);
+
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 10 }; //bu rastgele sıra ile yazılabilir.
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1); //burada product sınıfından oluşan nesnenin adresi gönderirilir aslında primitive tiplerde ise sadece değer gönderilir.
+            //Console.WriteLine(product1.ProductName); //kamera yazar çünkü nesnenin referansında değişiklik yapılır
+            
+            //int double bool ... değer tiplerdir
+            //diziler class abstract class interface .... referans tiplerdir
+            //ref ve out keywordleri ile değer tipleri ref tip gibi kullanabilirsin.
+
+
+        }
+    }
+}
