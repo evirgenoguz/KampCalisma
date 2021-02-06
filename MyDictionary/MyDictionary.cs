23d57da767af821f609f42a4d65b5028ef5afedf
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-02-06 20:36:02 +0300

    Kamp 4. Gün Dictionary Sınıfı Oluşturma Ödevi

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, V>
+    {
+        T[] itemKeys;
+        V[] itemValues;
+        public MyDictionary()
+        {
+            //keyleri ve valueleri ayrı ayrı dizilerde aynı indislerde tutacağım.
+            itemKeys = new T[0];
+            itemValues = new V[0];
+            Console.WriteLine("Constructor Çalıştı");
+        }
+        public void Add(T itemKey, V itemValue)
+        {
+            T[] tempItemKeys = itemKeys;
+            V[] tempItemValues = itemValues;
+
+            itemKeys = new T[itemKeys.Length + 1];
+            itemValues = new V[itemValues.Length + 1];
+
+            //Console.WriteLine(itemKeys.Length);
+
+            for (int i = 0; i < tempItemKeys.Length; i++)
+            {
+                itemKeys[i] = tempItemKeys[i];
+                itemValues[i] = tempItemValues[i];
+            }
+
+            itemKeys[itemKeys.Length - 1] = itemKey;
+            itemValues[itemValues.Length - 1] = itemValue;
+
+            
+            
+            Console.WriteLine("Eklenen");
+
+            Console.WriteLine("Key : " + itemKey + " Value : " + itemValue);
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<string, int> isimId = new MyDictionary<string, int>();
+            isimId.Add("Oguz", 1);
+        }
+    }
+}
