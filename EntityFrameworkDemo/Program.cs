5fd9fa37813b1823b8424f6f55241e6aea8306c2
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-02-28 14:48:52 +0300

    EntityFrameworkDemo

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entity Framework -- Bir ORM - Object Relational Mapping
+            //Alternatif olarak NHibernate
+            //Dapper ama biz entityframework kullanacağız.
+
+            NorthwindContext context = new NorthwindContext(); //db ile bağlandığımız class nesnesi
+
+            foreach (var product in context.Products)
+            {
+                Console.WriteLine(product.ProductName);
+                Console.WriteLine("******************");
+            }
+
+            GetProductsByCategoryId(2);
+   
+        }
+
+        private static void GetProductsByCategoryId(int categoryId)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            foreach (var product in context.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
