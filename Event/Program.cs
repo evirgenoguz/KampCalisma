af51e175faf29a541f8439cbcc88a07d78772137
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-03-21 15:16:15 +0300

    Events

---
+++ Event/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harddisk = new Product(50);
+            harddisk.ProductName = "Hard Disk";
+
+            Product gsm = new Product(50);
+
+
+        }
+    }
+}
